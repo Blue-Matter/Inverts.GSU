f7d3efba754e531d7294178db30b318571d20eb9
tcarruth <tomsinbox@gmail.com>
2025-02-05 17:40:19 -0800

    OMs

---
+++ R/fit_GSU.R
@@ -0,0 +1,34 @@
+#' Condition an operating model for Green Sea Urchin
+#'
+#' Uses the RCM model of OpenMSE to fit an operating model to data
+#'
+#' @param In A list object of class ('In') that includes the operating model parameters (slot 2, class OM) and data (slot 3, class RCMinput) for conditioning
+#' @param sims Integer or vector of integers - the number of simulations for the operating model (e.g. 96) or the specific vector of simulations for the operating model (e.g. 13:48)
+#' @param max_F Positive real number - the maximum instantaneous mortality rate in any historical time step
+#' @param comp_like The likelihood function used for composition data (age / length) c("multinomial", "lognormal", "mvlogistic", "dirmult1", "dirmult2") see ?RCM
+#' @param resample Logical - should parameters be drawn from the variance covariance matrix of the MLE fit (T) or just the MLE parameter values (F)
+#' @param silent Logical - should all RCM messages be repressed (ie not read out onto the console)?
+#' @examples
+#' cond.GSU("In.GSU.12")
+#' @author T. Carruthers
+#' @seealso \link{RCM}
+#' @export
+cond.GSU = function(RCMinput,sims = 12, max_F=3, comp_like="multinomial",resample = T, silent=T){
+  setup()
+  if(length(sims) == 1) simy = 1:sims
+  if(length(sims) > 1) simy = sims
+  OM = SubCpars(RCMinput$OM, simy)
+
+  RCMfit = RCM(OM, N_input[[3]], selectivity = "logistic_length",
+           s_selectivity = N_input[[3]]@Misc$s_selectivity,
+           start = list(ivul_par = N_input[[3]]@Misc$ivul_par),
+           map = list(ivul_par = N_input[[3]]@Misc$map_ivul_par),
+           max_F = maxF, mean_fit = T, condition = "catch",
+           cores = 8,  drop_nonconv=T,drop_highF=T,comp_like = comp_like, silent=silent)
+  RCMfit@OM@Name = paste0("Green sea urchin Stat. area ",RCMinput[[1]])
+  RCMfit
+
+}
+
+
+
