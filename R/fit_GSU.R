9b3227ac613e3b8fdad490595b3a0fd2c91e8687
tcarruth <tomsinbox@gmail.com>
2025-02-07 07:36:39 -0800

    parallel optional

--- R/fit_GSU.R
+++ R/fit_GSU.R
@@ -7,14 +7,18 @@
 #' @param max_F Positive real number - the maximum instantaneous mortality rate in any historical time step
 #' @param comp_like The likelihood function used for composition data (age / length) c("multinomial", "lognormal", "mvlogistic", "dirmult1", "dirmult2") see ?RCM
 #' @param resample Logical - should parameters be drawn from the variance covariance matrix of the MLE fit (T) or just the MLE parameter values (F)
+#' @param parallel Logical - should model fitting use multiple cores
 #' @param silent Logical - should all RCM messages be repressed (ie not read out onto the console)?
 #' @examples
 #' cond.GSU("In.GSU.12")
 #' @author T. Carruthers
 #' @seealso \link{RCM}
 #' @export
-cond.GSU = function(RCMinput, sims = 12, max_F=3, comp_like="multinomial", resample = T, silent=T){
-  setup()
+cond.GSU = function(RCMinput, sims = 12, max_F=3, comp_like="multinomial", resample = T, parallel=T, silent=T){
+  if(parallel){
+    setup()
+    cores=parallel::detectCores()/2
+  }
   if(length(sims) == 1) simy = 1:sims
   if(length(sims) > 1) simy = sims
   OM = SubCpars(RCMinput$OM, simy)
@@ -24,7 +28,7 @@ cond.GSU = function(RCMinput, sims = 12, max_F=3, comp_like="multinomial", resam
            start = list(ivul_par = RCMinput[[3]]@Misc$ivul_par),
            map = list(ivul_par = RCMinput[[3]]@Misc$map_ivul_par),
            max_F = max_F, mean_fit = T, condition = "catch",
-           cores = 8,  drop_nonconv=T,drop_highF=T,comp_like = comp_like, silent=silent)
+           cores = cores,  drop_nonconv=T,drop_highF=T,comp_like = comp_like, silent=silent)
   RCMfit@OM@Name = paste0("Green sea urchin Stat. area ",RCMinput[[1]])
   RCMfit
 
